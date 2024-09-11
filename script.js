de076c010d4874d4632af17f0350ffc4b77d412f
ashmit0611 <ashmitdudani06@gmail.com>
2024-09-12 03:20:50 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var typed = new Typed(".typing",{
-    strings:["Web Designer","Web Developer","Music Producer","Graphic Designer"],
+    strings:["Web Designer","Web Developer","Music Producer","Video Editor"],
     typeSpeed:100,
     BackSpeed:60,
     loop:true
